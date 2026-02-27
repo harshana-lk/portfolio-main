a56a0ed94562a1029e65f747209e923066f2d533
harshana-lk <harshuonlive@gmail.com>
2026-02-27 18:38:29 +0530

    Updaetd Dependencies

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
